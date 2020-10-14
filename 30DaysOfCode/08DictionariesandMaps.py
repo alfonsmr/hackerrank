a459292b84a9cd3235ac389b66b8e7006d1cdf70
Alfons <alfonsmr@gmail.com>
2020-10-14 17:43:15 -0500

    30 Days of Code - 12

--- 30DaysOfCode/08DictionariesandMaps.py
+++ 30DaysOfCode/08DictionariesandMaps.py
@@ -2,6 +2,12 @@
 
 #!/bin/python3
 
+# The first line contains an integer, , denoting the number of entries in the phone book.
+# Each of the  subsequent lines describes an entry in the form of  space-separated values on a single line.
+# The first value is a friend's name, and the second value is an -digit phone number.
+# After the  lines of phone book entries, there are an unknown number of lines of queries. 
+# Each line (query) contains a  to look up, and you must continue reading lines until there is no more input.
+
 n = int(input())
 name_numbers = [input().split() for _ in range(n)]
 phone_book = {k: v for k,v in name_numbers}
